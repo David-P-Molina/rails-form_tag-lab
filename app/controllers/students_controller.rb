0567eac0c9859ff263d66e4813d24bfc63fe567d
David Moina <davemolina777@gmail.com>
2021-03-11 14:03:38 -0600

    commit tree 73da251552503b1f8f96ef2add8450559ea93651

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,7 +5,13 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @student = Student.find(params[:id])
+    @student = Student.find_by(id: params[:id])
+  end
+  def new
+  end
+  def create
+    session[:student_params] = params.inspect
+    redirect_to new_student_path
   end
 
 end
